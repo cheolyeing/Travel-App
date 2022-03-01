96b18de10e873201e0c95f0c6c45bc898d792f3b
Limi <cheolyeing>
2022-03-01 14:52:06 +0900

    Apply SearchOptionContext to Home

--- App.tsx
+++ App.tsx
@@ -2,21 +2,25 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import HomeIcon from './assets/images/icon_home';
-import MyIcon from './assets/images/icon_my';
+import HomeIcon from './assets/images/icon_home.svg';
+import MyIcon from './assets/images/icon_my.svg';
 
-import Home from './src/component/Home';
-import My from './src/component/My';
+import Home from './src/components/Home';
+import My from './src/components/My';
+
+import {SearchOptionContextProvider} from './src/contexts/SearchOptionContext';
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} options={{tabBarIcon: ({color}) => <HomeIcon color={color} />}} />
-        <Tab.Screen name="My" component={My} options={{tabBarIcon: ({color}) => <MyIcon color={color} />}} />
-      </Tab.Navigator>
+      <SearchOptionContextProvider>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={Home} options={{tabBarIcon: ({color}) => <HomeIcon color={color} />}} />
+          <Tab.Screen name="My" component={My} options={{tabBarIcon: ({color}) => <MyIcon color={color} />}} />
+        </Tab.Navigator>
+      </SearchOptionContextProvider>
     </NavigationContainer>
   );
 };
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
+import {useSearchOptioncontext} from '../../contexts/SearchOptionContext';
+
 const Home = () => {
+  const {area, setArea, category, setCategory} = useSearchOptioncontext();
+
+  setTimeout(() => {
+    setArea?.('1초');
+  }, 1000);
+  setTimeout(() => {
+    setCategory?.('2초');
+  }, 2000);
+
   return (
     <View>
       <Text>홈</Text>
+      <Text>지역: {area}</Text>
+      <Text>카테고리: {category}</Text>
     </View>
   );
 };
